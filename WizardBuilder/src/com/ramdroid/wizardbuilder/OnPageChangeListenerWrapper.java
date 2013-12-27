49bc984b01d0063a14bc4d3a5252dd7384a0f71c
Andrew Chen <yongjhih@gmail.com>
2013-12-27 15:10:10 +0800

    Allow finish after last page swiped

---
+++ WizardBuilder/src/com/ramdroid/wizardbuilder/OnPageChangeListenerWrapper.java
@@ -0,0 +1,17 @@
+package com.ramdroid.wizardbuilder;
+
+import android.support.v4.view.ViewPager;
+
+public class OnPageChangeListenerWrapper implements ViewPager.OnPageChangeListener {
+    @Override
+    public void onPageSelected(int position) {
+    }
+
+    @Override
+    public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+    }
+
+    @Override
+    public void onPageScrollStateChanged(int state) {
+    }
+}
